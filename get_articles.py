0b2277bdd35409f50e8f617e71fcddea82b06f50
polar0n <igariac@gmail.com>
2024-09-26 23:25:48 +0300

    Adding metadata

---
+++ get_articles.py
@@ -0,0 +1,53 @@
+import aiohttp
+from bs4 import BeautifulSoup
+import asyncio
+import json
+from yarl import URL
+
+
+URLS = [
+    'https://edition.cnn.com/article/sitemap-2023-1.html',
+    'https://edition.cnn.com/article/sitemap-2023-2.html',
+    'https://edition.cnn.com/article/sitemap-2023-3.html',
+    'https://edition.cnn.com/article/sitemap-2023-4.html',
+    'https://edition.cnn.com/article/sitemap-2023-5.html',
+    'https://edition.cnn.com/article/sitemap-2023-6.html',
+    'https://edition.cnn.com/article/sitemap-2023-7.html',
+    'https://edition.cnn.com/article/sitemap-2023-8.html',
+    'https://edition.cnn.com/article/sitemap-2023-9.html',
+    'https://edition.cnn.com/article/sitemap-2023-10.html',
+    'https://edition.cnn.com/article/sitemap-2023-11.html',
+    'https://edition.cnn.com/article/sitemap-2023-12.html'
+]
+
+URLS = map(lambda url: URL(url), URLS)
+
+articles = list()
+
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        for url in URLS:
+            async with session.get(url) as response:
+                html = await response.text()
+                print(url)
+
+                soup = BeautifulSoup(html, 'lxml')
+                for element in soup.select('.sitemap-entry ul li'):
+                    href = element.a.get('href')
+                    active_url = URL(href)
+                    if active_url.parts[-3] == 'live-news':
+                        continue
+                    article = list()
+                    article.append(element.find(class_='date').text)
+                    article.append(element.a.text)
+                    article.append(href)
+                    article.append(active_url.parts[-2])
+                    article.append(active_url.parts[-3])
+                    articles.append(article)
+
+    articles = sorted(articles, key=lambda article: article[3])
+    with open('articles.json', 'w', encoding='utf-8') as f:
+        json.dump(articles, f, indent=2)
+
+asyncio.run(main())
--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let urls = new Array();
+let articles = new Array();
 document.getElementsByClassName('sitemap-entry')[1]
     .childNodes[0].childNodes
     .forEach(function (list_item) {
-        urls.push(list_item.lastChild.lastChild.href);
+        let article = new Array();
+        article.push(list_item.firstChild.textContent);
+        let el = list_item.lastChild.lastChild;
+        article.push(el.text);
+        article.push(el.href);
+        articles.push(article);
     });
-console.log(urls);
+console.log(articles);
